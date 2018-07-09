64a66846483cbe05fcf110257b4190c976d855ea
Mistergix <nicolas.ruche.entreprise@gmail.com>
2018-07-09 23:34:43 +0200

    landing page

---
+++ site/index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html class="no-js" lang="fr">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Accueil Déficode</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="apple-touch-icon" href="icon.png">
+  <!-- Place favicon.ico in the root directory -->
+
+  <link rel="stylesheet" href="../css/normalize.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+  <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+  <!--[if lte IE 9]>
+    <p class="browserupgrade">Vous utilisez  un navigateur <strong>obsolète</strong>. Veuillez <a href="https://browsehappy.com/">mettre votre navigateur à jour</a> pour améliorer votre expérience et votre sécurité.</p>
+  <![endif]-->
+  <nav class="navbar navbar-expand-xl navbar-light bg-light fixed-top">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand img-link" href="http://parkours.fr/"><img class="img-fluid" src="../img/coloredParkoursLogo.svg" alt="Logo Parkours coloré"></a>
+                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbar-collapse-main">
+                    <span class="navbar-toggler-icon">
+                    </span>
+                </button>
+            </div>
+            <div class="collapse navbar-collapse" id="navbar-collapse-main">
+                <ul class="nav navbar-nav m-auto">
+                    <li class="nav-item active"><a class="nav-link" href="#hello">ACCUEIL</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#concept">LES SEANCES</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#method">LES PROJETS</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#stages">COMPLEMENTS</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <article class="article">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-4 offset-lg-2">
+                    <h1>Le langage Python</h1>
+                    <p>
+                        On va programmer en Python. Il s'agit d'un langage de programmation professionnel.
+                    </p>
+                    <p>
+                        Il est utilisé par Google ou la NASA par exemple.
+                    </p>
+                    <p>
+                        Il est facile à utiliser.
+                    </p>
+                </div>
+                <div class="col-lg-4">
+                    <img class="img-fluid" src="../img/python_logo.png" alt="Logo Python">  
+                </div>
+            </div> <!-- END ROW -->
+        </div>
+    </article>
+
+</body>
+
+</html>
