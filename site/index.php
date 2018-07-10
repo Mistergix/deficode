ca9097f4ba243ae0789cabd2e01ce15fc3ccfe31
Mistergix <nicolas.ruche.entreprise@gmail.com>
2018-07-10 18:21:34 +0200

    home page made without footer

--- site/index.php
+++ site/index.php
@@ -2,30 +2,33 @@
 <html class="no-js" lang="fr">
 
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <title>Accueil Déficode</title>
-  <meta name="description" content="">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Accueil Déficode</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <link rel="manifest" href="site.webmanifest">
-  <link rel="apple-touch-icon" href="icon.png">
-  <!-- Place favicon.ico in the root directory -->
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="apple-touch-icon" href="icon.png">
+    <!-- Place favicon.ico in the root directory -->
 
-  <link rel="stylesheet" href="../css/normalize.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/normalize.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+        crossorigin="anonymous">
 
-  <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 
 <body>
-  <!--[if lte IE 9]>
+    <!--[if lte IE 9]>
     <p class="browserupgrade">Vous utilisez  un navigateur <strong>obsolète</strong>. Veuillez <a href="https://browsehappy.com/">mettre votre navigateur à jour</a> pour améliorer votre expérience et votre sécurité.</p>
   <![endif]-->
-  <nav class="navbar navbar-expand-xl navbar-light bg-light fixed-top">
+    <nav class="navbar navbar-expand-xl navbar-light bg-light fixed-top">
         <div class="container-fluid">
             <div class="navbar-header">
-                <a class="navbar-brand img-link" href="http://parkours.fr/"><img class="img-fluid" src="../img/coloredParkoursLogo.svg" alt="Logo Parkours coloré"></a>
+                <a class="navbar-brand img-link" href="http://parkours.fr/">
+                    <img class="img-fluid" src="../img/coloredParkoursLogo.svg" alt="Logo Parkours coloré">
+                </a>
                 <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbar-collapse-main">
                     <span class="navbar-toggler-icon">
                     </span>
@@ -33,37 +36,79 @@
             </div>
             <div class="collapse navbar-collapse" id="navbar-collapse-main">
                 <ul class="nav navbar-nav m-auto">
-                    <li class="nav-item active"><a class="nav-link" href="#hello">ACCUEIL</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#concept">LES SEANCES</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#method">LES PROJETS</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#stages">COMPLEMENTS</a></li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#hello">ACCUEIL</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#concept">LES SEANCES</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#method">LES PROJETS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#stages">COMPLEMENTS</a>
+                    </li>
                 </ul>
             </div>
         </div>
     </nav>
 
-    <article class="article">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-lg-4 offset-lg-2">
-                    <h1>Le langage Python</h1>
-                    <p>
-                        On va programmer en Python. Il s'agit d'un langage de programmation professionnel.
-                    </p>
-                    <p>
-                        Il est utilisé par Google ou la NASA par exemple.
-                    </p>
-                    <p>
-                        Il est facile à utiliser.
-                    </p>
-                </div>
-                <div class="col-lg-4">
-                    <img class="img-fluid" src="../img/python_logo.png" alt="Logo Python">  
-                </div>
-            </div> <!-- END ROW -->
-        </div>
-    </article>
+    <div class="content">
+        <article class="article content-main">
+            <div class="container-fluid">
+                <section class="row">
+                    <div class="col-lg-4 offset-lg-2 column">
+                        <figure>
+                            <img class="img-fluid" src="../img/python_logo.svg" alt="Logo de Python">
+                        </figure>
+                        <h1 class="title">Le langage Python</h1>
+                        <p>
+                            Durant cette année on va programmer en Python, un langage professionnel.
+                        </p>
+                        <h2 class="title">Ce que l'on peut faire en Python</h2>
+                        <ul>
+                            <li>Des sites webs</li>
+                            <li>Des jeux vidéo</li>
+                            <li>Programmer des robots</li>
+                            <li>De la science</li>
+                        </ul>
+                        <h2 class="title">Qui utilise Python</h2>
+                        <ul>
+                            <li>Google</li>
+                            <li>Facebook</li>
+                            <li>NASA</li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-4 column">
+                        <figure>
+                            <img class="img-fluid" src="../img/parkours_logo.svg" alt="Logo de Parkours">
+                        </figure>
+                        <h1 class="title">Parkours et Déficode</h1>
+                        <p>
+                            Ce cours, créé pour
+                            <a href="http://parkours.fr/">Parkours</a>, te permettra d'apprendre à programmer.
+                        </p>
+                        <h2 class="title">Programme du Déficode</h2>
+                        <ul>
+                            <li>Le langage Python</li>
+                            <li>Des outils professionnels comme Git et Github</li>
+                            <li>Jeux vidéo avec Pygame, Tiled, Bosca CEOIL et TexturePacker</li>
+                            <li>Un projet personnalisé</li>
+                        </ul>
+                        <h2 class="title">Le site</h2>
+                        <ul>
+                            <li>Un résumé des notions</li>
+                            <li>La liste des projets effectués durant l'année</li>
+                            <li>Des ressources complémentaires</li>
+                        </ul>
+                    </div>
+                </section>
+                <!-- END ROW -->
+            </div>
+        </article>
+        <footer>
 
+        </footer>
+    </div>
 </body>
-
 </html>
