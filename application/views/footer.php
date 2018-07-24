833ace9cd4f601d881668f2068c6d67f2529638b
Mistergix <nicolas.ruche.entreprise@gmail.com>
2018-07-24 22:21:12 +0200

    enhanced links

--- application/views/footer.php
+++ application/views/footer.php
@@ -2,7 +2,7 @@
     <div class="container-fluid">
       <div class="row">
         <div class="col-lg-4 offset-lg-2">
-          <a href="<?php echo base_url("humans.txt");?>">Un peu d'infos sur le site</a>
+          <a href="<?php echo base_url("humans.txt");?>" target="_blank">Un peu d'infos sur le site</a>
         </div>
         <div class="col-lg-4">
           <a href="<?php echo site_url("contact");?>">Contacts</a>
