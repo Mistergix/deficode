569c64be47f962d2b02cb49d2ba760327a3e4deb
Nikow <nicolas.ruche.entreprise@gmail.com>
2018-12-01 17:50:20 +0100

    removing form

--- dist/application/controllers/Registerchild.php
+++ dist/application/controllers/Registerchild.php
@@ -7,25 +7,6 @@ class Registerchild extends Basecontroller
 {
 	public function index()
 	{
-		$this->load->helper(array('form'));
-		$this->load->library('form_validation');
-
-		$this->form_validation->set_rules('parent_fn', 'Votre prénom', 'required|trim');
-        $this->form_validation->set_rules('parent_ln', 'Votre nom', 'required|trim');
-        $this->form_validation->set_rules('parent_email', 'Votre email', 'required|trim|valid_email');
-		$this->form_validation->set_rules('parent_phone', 'Votre numéro de téléphone portable', 'required|trim|regex_match[/^0[1-9](?:\d{2}){4}$/]');
-		$this->form_validation->set_rules('child_fn', 'Prénom de votre enfant', 'required|trim');
-		$this->form_validation->set_rules('child_ln', 'Nom de votre enfant', 'required|trim');
-		$this->form_validation->set_rules('child_level', 'Classe de votre enfant', 'required|trim');
-
-
-		if ($this->form_validation->run() == FALSE)
-		{
-			$this->load_page("forms/register_child/form", ["title" => "Inscrire mon enfant", "styles" => ["main", "form"], "scripts" => ["main", "validate"]]);
-		}
-		else
-		{
-			$this->load_page("forms/register_child/success", ["title" => "Mon enfant est inscrit", "styles" => ["main"], "scripts" => ["main"]]);
-		}
+		redirect();
 	}
 }
--- dist/application/views/forms/register_child/success.php
+++
@@ -1,7 +0,0 @@
-<h3>Votre enfant a été inscrit !</h3>
-
-<p>
-	Nous vous enverrons bientôt un mail de confirmation.
-</p>
-
-<p><?= anchor('registerchild', 'Essayez encore'); ?></p>
