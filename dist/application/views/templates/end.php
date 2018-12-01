7d1224540daf10a63b57706ffe5a5397c690d23f
Nikow <nicolas.ruche.entreprise@gmail.com>
2018-12-01 15:48:16 +0100

    a bit of form

--- dist/application/views/templates/end.php
+++ dist/application/views/templates/end.php
@@ -4,6 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 </div> <!-- END PAGE_WRAPPER -->
 <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-<script src="<?php echo js_url("main"); ?>"></script>
+<script src="<?= js_url("main"); ?>"></script>
 </body>
 </html>
