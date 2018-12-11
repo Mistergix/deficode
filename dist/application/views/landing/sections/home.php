c95edf524422030eb388491ba8ab5d06a3a10f2b
Nikow <nicolas.ruche.entreprise@gmail.com>
2018-12-12 00:38:10 +0100

    styling

---
+++ dist/application/views/landing/sections/home.php
@@ -0,0 +1,18 @@
+<div class="centered">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-12">
+				<h1>Déficode</h1>
+			</div>
+			<div class="col-12 mt-3">
+				<h3>L'informatique au collège</h3>
+			</div>
+			<div class="col-12 mt-3">
+				<a href="#" class="btn btn-primary btn-block">Inscrire mon enfant</a>
+			</div>
+			<div class="col-12 mt-3">
+				<a href="#" class="btn btn-primary btn-block">Espace élèves</a>
+			</div>
+		</div>
+	</div>
+</div>
