ff8122749817a2f99e6c51ccdc54e9510cea4db9
Nikow <nicolas.ruche.entreprise@gmail.com>
2018-12-15 09:49:36 +0100

    responsive contact

--- dist/application/views/landing/sections/contact.php
+++ dist/application/views/landing/sections/contact.php
@@ -5,7 +5,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 <div class="container-fluid">
 <h3>Nous contacter</h3>
 <div class="row">
-		<div class="col-12">
+		<div class="col-12 col-md-6 offset-md-3">
 			<a href="<?= site_url("contact");?>" target="_blank" rel="noopener noreferrer" class="btn btn-primary btn-block">Laissez-nous un petit mot !</a>
 		</div>
 </div><!-- END ROW --> 
