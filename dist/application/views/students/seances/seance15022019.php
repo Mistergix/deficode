232f804b7f2a00d78a09b834bca8fec6439280b3
Nikow <nicolas.ruche.entreprise@gmail.com>
2019-02-14 12:53:38 +0100

    added the recap

--- dist/application/views/students/seances/seance15022019.php
+++ dist/application/views/students/seances/seance15022019.php
@@ -1,3 +1,13 @@
 <?php
-defined('BASEPATH') or exit('No direct script access allowed');
+defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
+
+<main class="container-fluid below-menu" id="main">
+	<div class="container">
+		<div class="row white-box">
+			<div class="col-12">
+			</div>
+		</div>
+
+	</div>
+</main>
