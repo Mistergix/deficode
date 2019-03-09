b8d3c50b1c223ce885b9449b541b4ca6c6e5eaf3
Nikow <nicolas.ruche.entreprise@gmail.com>
2019-03-09 18:27:39 +0100

    finished with images

--- dist/application/views/students/seances/seance15032019.php
+++ dist/application/views/students/seances/seance15032019.php
@@ -55,15 +55,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<div class="row white-box">
 			<div class="col-12">
 				<h1>Commençons !</h1>
+				<p class="warning">
+						Il ne doit pas y a voir d'espace, ni d'accents dans les noms de fichiers/dossiers.
+				</p>
 				<div class="exercice">
 					<p>
 						Ouvre l'éditeur de texte <b>Brackets</b>.
 					</p>
 					<p>
 						Crée un nouveau dossier <span class="file">monsite</span>.
-					</p>
-					<p class="warning">
-						Il ne doit pas y a voir d'espace, ni d'accents dans les noms de fichiers/dossiers.
 					</p>
 					<p>
 						Crée dans ce dossier un fichier <span class="file">index.html</span>. 
@@ -79,11 +79,18 @@ https://www.google.fr
 						</pre>
 					</div>
 					<p>
+						Sauvegarde ton fichier.
+					</p>
+					<p>
 						Ouvre <span class="file">index.html</span> avec ton <b>navigateur</b>.
 					</p>
+					<p>
+						Bravo tu as écrit ta première page HTML !
+					</p>
 				</div>
-				<p>
-					Bravo tu as écrit ta première page HTML !
+				<p class="warning">
+					A chaque fois que tu fais des modifications sur tes fichiers tu dois sauvegarder. Puis tu dois actualiser la page 
+					sur ton navigateur, sinon tu ne verras pas les modifications.
 				</p>
 				<p>
 					Evidemment, ton site n'est pas en ligne, il est <b>local</b>. 
@@ -244,7 +251,7 @@ https://www.google.fr
 					la plus récente d'HTML.
 				</p>
 				<p class="warning">
-					Il n'y a pas de balise fermante pour le DOCTYPE.
+					Il n'y a pas de balise fermante pour le DOCTYPE. Cela s'appelle une balise <b>orpheline</b>.
 				</p>
 				<p>
 					La seconde balise est <code>&lt;html&gt;&lt;/html&gt;</code>. Elle contient <b>toute</b> la page. 
@@ -254,6 +261,9 @@ https://www.google.fr
 					Le <code>&lt;head&gt;&lt;/head&gt;</code> va contenir des informations utiles sur la page. Ici par exemple elle contient
 					son <b>titre</b> qui va s'afficher dans l'onglet. 
 					La balise <code>&lt;meta charset="utf-8"&gt;</code> va nous permettre d'utiliser des accents.
+				</p>
+				<p class="warning">
+					La balise <code>meta</code> est aussi orpheline.
 				</p>
 				<p>
 					Enfin, la balise <code>&lt;body&gt;&lt;/body&gt;</code> va contenir la partie que l'on veut montrer aux visiteurs :
@@ -276,11 +286,179 @@ https://www.google.fr
 					<p>
 						Regarde le résultat dans le navigateur et regarde le nom de ton onglet, à quoi corresdond-til ?
 					</p>
+					<p>
+						Rends-toi sur ces 3 sites, affiche leur code source et cherche leur DOCTYPE, leur balise <code>head</code> et <code>body</code> :
+					</p>
+					<ul>
+						<li><a href="https://www.w3schools.com/" target="_blank" rel="noopener noreferrer">Lien 1</a></li>
+						<li><a href="https://basecamp.com/" target="_blank" rel="noopener noreferrer">Lien 2</a></li>
+						<li><a href="https://github.com/" target="_blank" rel="noopener noreferrer">Lien 3</a></li>
+					</ul>
 				</div>
 				<p>
-					A chaque fois que tu veux créer une page web, tu peux télécharger <a href="<?= code_url("seance8/template", "html");?>" download="template.html">ce template</a> qui contient
+					A chaque fois que tu veux créer une page web, tu peux télécharger
+					<a href="<?= code_url("seance8/template", "html");?>" download="template.html">ce template</a> qui contient
 					une page HTML de base.
 				</p>
+			</div>
+		</div>
+
+		<div class="row white-box">
+			<div class="col-12">
+				<h1>Un peu plus sur les liens</h1>
+				<p>
+					Dans la balise <code>a</code>, le "https://www.youtube.com/" s'appelle un <b>chemin</b> ou une <b>URL</b>.
+				</p>
+				<p>
+					Il existe deux types de chemins : <b>absolu</b> et <b>relatif</b>.
+				</p>
+				<p>
+					"https://www.youtube.com/" est un chemin absolu. On utilise les chemins absolus pour référencer des ressources sur d'autres sites. Ici je veux rediriger vers un autre site : Youtube.
+				</p>
+				<p>
+					Si je veux utiliser des ressources qui se trouvent sur le site que l'on crée, on va utiliser des chemins relatifs.
+				</p>
+				<p>
+					Les chemins relatifs dépendent de comment tu organises tes dossiers. Par exemple on va prendre cette structure :
+				</p>
+				<div class="row">
+					<div class="col-12 col-md-6 col-lg-4 offset-lg-4 offset-md-3">
+						<img class="img-fluid" src="<?=img_url("seance8/dossiers")?>" alt="Structures de dossier">
+					</div>
+				</div>
+
+				<h2>Pour un fichier au même niveau</h2>
+				<p>
+					Si depuis <span class="file">index.html</span> je veux aller dans <span class="file">cv.html</span>, le
+					chemin est simplement <code>cv.html</code>.
+				</p>
+				<p class="example">
+					Mon lien sera par exemple : <code>&lt;a href="cv.html"&gt;Voir mon CV&lt;/a&gt;</code>
+				</p>
+
+				<h2>Pour un fichier dans un dossier enfant</h2>
+				<p>
+					Si depuis <span class="file">index.html</span> je veux aller dans <span class="file">article.html</span>, je dois
+					passer par le dossier <span class="file">articles</span>. Le chemin sera donc <code>articles/article.html</code>.
+				</p>
+				<p class="example">
+					Mon lien sera par exemple : <code>&lt;a href="articles/article.html"&gt;Voir mon article&lt;/a&gt;</code>
+				</p>
+				<p>
+					S'il y a d'autres sous-dossiers, je dois les inclure.
+				</p>
+				<p class="example">
+					Par exemple : <code>dossier1/dossier2/dossier3/article.html</code>
+				</p>
+
+				<h2>Pour un fichier dans un dossier parent</h2>
+				<p>
+					Si je pars de <span class="file">article.html</span> pour aller à <span class="file">index.html</span>, je dois remonter, on écrira : 
+					<code>../index.html</code>
+				</p>
+				<p class="example">
+					Mon lien sera par exemple : <code>&lt;a href="../index.html"&gt;Retour à l'accueil&lt;/a&gt;</code>
+				</p>
+				<p>
+					S'il y a d'autres dossiers, je dois les inclure.
+				</p>
+				<p class="example">
+					Par exemple : <code>../../../index.html</code>
+				</p>
+				<p class="example">
+					Il est possible d'avoir des chemins comme ça : <br>
+					<code>../../../dossier1/dossier2/index.html</code>
+				</p>
+
+				<div class="exercice">
+					<p>
+						Voici un exercice pour comprendre les chemins relatifs :
+					</p>
+					<p>
+						Dans ton dossier <span class="file">monsite</span>, crée un fichier nommé <span class="file">cv.html</span>.
+					</p>
+					<p>
+						Dans ton dossier <span class="file">monsite</span>, crée un dossier <span class="file">articles</span>.
+					</p>
+					<p>
+						Télécharge <a href="<?= code_url("seance8/rat-taupe-nu", "html");?>" download="article.html">ce fichier</a> 
+						et place le dans le dossier <span class="file">articles</span>.
+					</p>
+					<p>
+						Sur ton fichier <span class="file">index.html</span>, rajoute ces liens :
+					</p>
+					<div class="code-box">
+						<pre>
+&lt;a href="cv.html"&gt;Voir mon CV&lt;/a&gt;
+&lt;a href="articles/article.html"&gt;Voir mon article&lt;/a&gt;
+						</pre>
+					</div>
+					<p>
+						Sur tes fichiers <span class="file">article.html</span> et <span class="file">cv.html</span>, rajoute ce lien :
+					</p>
+					<div class="code-box">
+						<pre>
+&lt;a href="../index.html"&gt;Retour à l'accueil&lt;/a&gt;
+						</pre>
+					</div>
+					<p>
+						Clique sur les liens dans ton navigateur.
+					</p>
+				</div>
+
+			</div>
+		</div>
+
+		<div class="row white-box">
+			<div class="col-12">
+				<h1>Les images</h1>
+				<p>
+					Grâce au HTML, on peut intégrer des images dans sa page web. 
+					Il y a 3 types d'images supportées par tous les navigateurs :
+				</p>
+				<ul>
+					<li>Les JPEG : Utilisées pour les photos (extension : .jpg ou .jpeg)</li>
+					<li>Les GIF : connus pour être des images animées (extension : .gif)</li>
+					<li>Les PNG : pour tout ce qui n'est pas une photo, on peut créer des images transparentes avec (extension : .png)</li>
+				</ul>
+				<p>
+					La balise pour intégrer une image est la balise <code>img</code>. C'est une balise <b>orpheline</b>. Voici le format :
+				</p>
+				<p>
+					<code>&lt;img src="[lien]" alt="[texte_alternatif]"&gt;</code>
+				</p>
+				<p>
+					L'attribut <code>alt</code> contient une petite description de l'image. C'est important car si l'image n'existe pas, c'est cette description qui s'affiche.
+				</p>
+				<p>
+					L'attribut <code>src</code> indique l'endroit ou se trouve l'image. Comme pour <code>href</code>, on peut y mettre un chemin absolu ou relatif.
+				</p>
+				<p class="example">
+					<code>&lt;img src="mes_images/animaux/gerbille.jpg" alt="Image d'une gerbille dans le désert"&gt;</code>
+				</p>
+
+				<div class="exercice">
+					<p>
+						Dans ton dossier <span class="file">monsite</span>, crée un nouveau dossier appelé <span class="file">images</span>.
+					</p>
+					<p>
+						Télécharge ces 3 images et place les dans ce dossier :
+					</p>
+					<ul>
+						<li><a href="<?=img_url("seance8/sucre", "jpg")?>" download="sucre.jpg">Image JPEG</a></li>
+						<li><a href="<?=img_url("seance8/earth", "gif")?>" download="terre.gif">Image GIF</a></li>
+						<li><a href="<?=img_url("seance8/moodboard")?>" download="moodboard.png">Image PNG</a></li>
+					</ul>
+					<p>
+						Dans ton fichier <span class="file">index.html</span>, intègre ces 3 images avec des 3 balises <code>img</code>. Utilise un chemin relatif et mets des descriptions.
+					</p>
+					<p>
+						Rajoute ceci à la suite de tes images : <code>&lt;img src="image_qui_nexiste_pas.png" alt="Une image qui n'existe pas"&gt;</code>
+					</p>
+					<p>
+						Regarde le résultat dans ton navigateur.
+					</p>
+				</div>
 			</div>
 		</div>
 
---
+++ dist/application/views/students/templates/objectif.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<a href="#">A PROPOS</a>
+	<a href="#">PORTFOLIO</a>
+	<a href="#">NEWSLETTER</a>
+	<a href="#">RESEAUX SOCIAUX</a>
+</body>
+</html>
