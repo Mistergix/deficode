895354eb999e1e25976a4a0485345b47efcdafac
Nikow <nicolas.ruche.entreprise@gmail.com>
2019-03-21 20:53:32 +0100

    changed layout

--- dist/application/views/students/landing_page.php
+++ dist/application/views/students/landing_page.php
@@ -8,7 +8,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			<div class="offset-lg-0"></div>
 			<?php foreach ($menu_links as $label => $link_data): ?>
 			<?php if($link_data['url'] != ""):?>
-			<div class="col-12 mt-3 col-md-4 col-lg-4">
+			<div class="col-12 mt-3 col-md-6 col-lg-6">
 				<a href="<?= site_url($link_data['url']); ?>" class="btn btn-primary btn-block"><?= $label ?></a>
 			</div>
 			<?php endif;?>
