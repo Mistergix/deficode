558310bbb9ddf3bbd2609467c14d8d3111f8079e
Julian M. <julian.merle-remond@devmares.net>
2019-03-29 14:25:54 +0100

    1ere partie manque 2 trucs que j'ai relégués à la fin

--- dist/application/views/students/seances/seance29032019.php
+++ dist/application/views/students/seances/seance29032019.php
@@ -66,7 +66,10 @@ p {
 				<p class="exercice">
 					Teste ce css.
 				</p>
-
+            </div>
+        </div>
+        <div class="row white-box">
+			<div class="col-12">
 				<h2>Manipulation de texte</h2>
 				<p>
 					On peut manipuler du texte à travers (notament) ces propriétés :
@@ -99,15 +102,111 @@ p {
 
 		<div class="row white-box">
 			<div class="col-12">
-				<h1>Les polices de caractères</h1>
+                <h1>Les polices de caractères</h1>
+                <p>
+                    Une police de caractères (ou police d'écriture) dit aux ordinateurs comment dessiner les lettres d'un texte.
+                </p>
+                <div class="example">
+					<p>écrire <b style="font-family: Serif">avec empatemment</b> ou <b style="font-family: cursive">en attaché</b></p>
+				</div>
+                <p>
+                    Pour choisir les polices de ses textes, on utilise le css: <code>font-family</code>.
+                </p>
 				<p>
-				<!-- 
-					TODO : 
-					- Expliquer rapidement ce qu'est une police 
-					- Leur montrer la propiété CSS font-family avec quelques polices : Helvetica, Arial etc.
-					- Leur montrer comment marche Google fonts ( choix d'une ou plusieurs polices, insertion de la 
-					balise <link>, utilisation dans le CSS)
-				-->
+                     Il permet de changer de police. Mais aussi de proposer des polices de remplacement si le navigateur ne trouve pas la police demandée: pour se faire, on ajoute d'autres noms de polices ou de types de police à la suite.
+                    <br>Valeurs : <code>'Nom de police'</code> ou <code>'type de police'</code> séparés par des virgules.
+                </p>
+				<div class="example">
+					<div class="code-box">
+						<pre>
+p {
+	font-family: 'Helvetica', Arial, Sans-serif;
+}
+						</pre>
+					</div>
+                    <p>
+                        Ici, on veut de l'Helvetica. Si il n'y en a pas, il va chercher de l'Arial. Et si il ne trouve toujours pas. Il sélection une police sans empattements disponible sur l'ordinateur.
+                    </p>
+				</div>
+                <p>
+                    Les <code>'</code> autour du nom de police ne sont nécessaire que si le nom continent des espaces ou autres caractères spéciaux.
+                </p>
+				<p class="exercice">
+					Teste le css de l'exemple!
+				</p>
+                <p>A la question: "Il y a quoi comme police qu'on peut mettre?", 
+                    on vous répond: <a href="https://fonts.google.com/" >GOOGLE FONTS</a>! <br>
+                    Rends-toi y et on t'explique comment faire!</p>
+                
+                <div class="col-12 col-md-10 offset-md-1">
+                    <img class="img-fluid" src="<?=img_url("seance10/gfont1", "jpg");?>" alt="illustration google fonts 1">
+                </div>
+                <ol>
+                    <li>
+                        La case "Search" entourée en fluo te permet de chercher un nom de police. Si tu ne sait pas quoi prendre, navigues dans les catégories (sous la barre "Search").
+                    </li>
+                    <li>
+                        Pour ajouter une police qui te plait, clique le bouton <i style="color: blue">"+" entouré en bleu</i> à coté de ta police. <br>
+                        Tu peux ajouter autant de polices que tu veux!
+                    </li>
+                    <li>
+                        Cliques sur "Families selected" <i style="color: green">(flèche verte)</i>.
+                    </li>
+                </ol>
+                <div class="col-12 col-md-10 offset-md-1">
+                    <img class="img-fluid" src="<?=img_url("seance10/gfont2", "jpg");?>" alt="illustration google fonts 2">
+                </div>
+                <ol start="4">
+                    <li>
+                        Copies ensuite ce qui est <i style="color: red">entouré en rouge</i> dans la section <code>&lt;head&gt;</code> de ta page web
+                    </li>
+                    <li>
+                        Utilises les lignes de CSS pointées en bleue dans ton code CSS pour voir la police s'afficher!
+                    </li>
+                </ol>
+                <p style="font-size: 0.8rem">
+                    Tu peux aussi télécharger ta police sur ton pc pour la tester ou t'en servir dans des documents <i style="color: limegreen">en cliquant sur l'icone entourée en vert</i>; c'est utile pour faire de jolies diapositives!
+                </p>
+                <p>
+                    Additionnellement, Google Fonts permet aussi de prendre les variantes en gras ou en italique des polices en allant dans l'onglet "CUSTOMIZE".
+                </p>
+				<div class="example">
+                    <p>
+                        Page HTML:
+                    </p>
+					<div class="code-box">
+						<pre>
+&lt;!DOCTYPE html&gt;
+&lt;html&gt;
+
+	&lt;head&gt;
+		&lt;meta charset="utf-8"&gt;				
+		&lt;title&gt;xXxCoolZoneDu92xXx&lt;/title&gt;
+		<b>&lt;link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet"&gt;</b>
+		&lt;link rel="stylesheet" href="css/style.css"&gt;
+	&lt;/head&gt;
+	
+	&lt;body&gt;
+		&lt;p&gt;Page riche en contenu intéressant&lt;/p&gt;
+	&lt;/body&gt;
+&lt;/html&gt;
+						</pre>
+					</div>
+                    <p>
+                        Dans le css/style.css de la page:
+                    </p>
+					<div class="code-box">
+						<pre>
+p {
+	font-size:      2em;
+	<b>font-family:   'Indie Flower', cursive;</b>
+}
+						</pre>
+					</div>
+                </div>
+                <p class="exercice">
+					Rajoute ce qui est en <b>gras</b> dans ton site d'après l'exemple!
+                    <br>Choisis ta police!
 				</p>
 			</div>
 		</div>
@@ -115,14 +214,161 @@ p {
 		<div class="row white-box">
 			<div class="col-12">
 				<h1>Les balises div</h1>
-				<p>
-				<!-- 
-					TODO : 
-					- Leur expliquer que pour créer un site, on doit ranger chaque élément dans div
-					- Leur montrer une photo d'un site sur lesquels les div sont entourés
-					- leur parler de la balise div
-				-->
+                <blockquote cite="<?=site_url("/seances/seance/seance29032019")?>" style="background: #eee; border-radius: 5px; padding: 0.5em; margin: 2em">
+                    <p>
+                        Aux débuts du Web, le W3C créa le <code>&lt;div&gt;</code>. Et les Webmasters furent libre de de <code>div</code>iser leur site en sections.
+                    </p>
+                    <cite>- Nous, Déficode 2019, colorisé</cite>
+                </blockquote>
+                <p>
+                    Les balises <code>&lt;div&gt;</code> permettent de définir des sections dans un site. On les rends plus spécifiques en leur attribuant une <code>class</code> ou un <code>id</code>.
+                </p>
+                <p>
+                    Cela permet d'arranger des éléments sur la page avec le CSS.
+                </p>
+                <div class="example">
+                    <p>
+                        Exemple de div sur <a href="<?=site_url("/templates/template/objectif")?>" >le site "objectif"</a>:
+                    </p>
+                    <div class="col-12 col-md-10 offset-md-1">
+                        <img class="img-fluid" src="<?=img_url("seance10/divs");?>" alt="D.I.V. (Directon Interdépartementale des Vol-au-vent)">
+                    </div>
+                    <p>
+                        En HTML:
+                    </p>
+					<div class="code-box">
+						<pre>
+&lt;div id="services"&gt;
+    &lt;div class="services_item"&gt;
+        &lt;i class="far fa-calendar-alt fa-4x"&gt;&lt;/i&gt;
+        &lt;p&gt;
+            J'organise plein de faux événements : raclette dans le Sahara, manifestation contre les manifestations en Corée du Nord, chasse à la loutre en Russie, end of thinking capacity. Voici la &lt;a href="#"&gt;liste des événements&lt;/a&gt; à ne pas manquer !
+        &lt;/p&gt;
+    &lt;/div&gt;
+    &lt;div class="services_item"&gt;
+        &lt;i class="fas fa-utensils fa-4x"&gt;&lt;/i&gt;
+        &lt;p&gt;
+            Je tiens un restaurant où tu peux manger : du boudin de gerbilles, du steack de bambous, de la cuisse de koala au miel, etc. La carte de &lt;a href="#"&gt;mon restaurant&lt;/a&gt; !
+        &lt;/p&gt;
+    &lt;/div&gt;
+    &lt;div class="services_item"&gt;
+        &lt;i class="fas fa-couch fa-4x"&gt;&lt;/i&gt;
+        &lt;p&gt;
+            J'ai créé une boutique de meubles faits de : bouteilles en plastique, brindilles, poussière et de chewing-gums mâchés récupérés sur le sol du métro. La liste des &lt;a href="#"&gt;meubles&lt;/a&gt; de ma boutique !
+        &lt;/p&gt;
+    &lt;/div&gt;
+&lt;/div&gt;
+                        </pre>
+                    </div>
+                </div>
+                <p class="exercice">
+					Essaie de structurer tes paragraphes avec des <code>&lt;div&gt;</code>!
 				</p>
+			</div>
+		</div>
+
+		<div class="row white-box">
+			<div class="col-12">
+				<h1>Les balises HTML 5</h1>
+				<p>
+					Le HMTL évolue au fil du temps, la dernière norme est "HTML 5". Elle ajoute entre-autres ces balises afin de limiter l'utilisation de div et donner plus de sens aux structures des pages web:
+				</p>
+				<ul>
+                    <li><code>&lt;main&gt;</code><br> Il groupe les éléments les plus importants qui sont souvent exclusifs à cette page web.</li>
+                    <li><code>&lt;section&gt;</code><br>
+                        Il groupe les éléments réalisant une fonction du site. Par exemple: la  recherche d'horaire d'un site de transports en commun</li>
+                    <li><code>&lt;article&gt;</code><br>
+                        Portion de page qui a un sens même sans lire le reste de la page web</li>
+                    <li><code>&lt;nav&gt;</code><br>
+                        Possède des liens vers d'autres portions du site ou de la page elle-même. Dans un <code>&lt;article&gt;</code>, cette section permettrait de naviguer entre ses paragraphes. </li>
+                    <li><code>&lt;aside&gt;</code><br>
+                        Section dont le contenu est un complément par rapport à ce qui l'entoure, qui n'est pas forcément en lien direct avec le contenu mais qui peut apporter des informations supplémentaires.</li>
+                    <li><code>&lt;header&gt;</code><br>
+                        Section d'introduction d'un article, d'une autre section ou du document entier (en-tête de page).</li>
+                    <li><code>&lt;footer&gt;</code><br>
+                        Section de conclusion d'une section ou d'un article, voire du document entier (pied de page).</li>
+				</ul>
+                <div class="example">
+					<div class="code-box">
+						<pre>
+&lt;body&gt;
+    &lt;header&gt;
+    &lt;h1&gt;Mon super site&lt;/h1&gt;
+    &lt;div id="SuperBanière"&gt;&lt;/div&gt;
+    &lt;p&gt;Rencontre du W-T-F&lt;/p&gt;
+    &lt;nav&gt;
+        &lt;ul&gt;
+            &lt;li&gt;lien vers un autre endroit du site&lt;/li&gt;
+            &lt;li&gt;chez Momo&lt;/li&gt;
+            &lt;li&gt;echange de billes&lt;/li&gt;
+        &lt;/ul&gt;
+    &lt;/nav&gt;
+    &lt;/header&gt;
+    
+    &lt;main&gt;
+        &lt;section&gt;
+            &lt;h1&gt; Tutos de JM&lt;/h1&gt;
+            &lt;article&gt;
+            
+                &lt;header&gt;
+                    &lt;h1&gt;Faire du lolz avec les pc&lt;/h1&gt;
+                &lt;/header&gt;
+                &lt;main&gt;
+                    &lt;h2 id="art-1"&gt;p c'est la life&lt;/h2&gt;
+                    &lt;p&gt;ppp&lt;/p&gt;
+                    &lt;h2 id="art-2"&gt;p c'est la life 2, le retour du roi&lt;/h2&gt;
+                    &lt;p&gt;ppp ppp&lt;/p&gt;
+                &lt;/main&gt;
+                &lt;aside&gt;
+                    &lt;h1&gt;Fantastique auteur de l'article&lt;/h1&gt;
+                    &lt;h2&gt;Jean MICHEL&lt;/h2&gt;
+                    &lt;div id="imageDeJeanMich"&gt;&lt;/div&gt;
+                    &lt;p&gt;Je fais du web. . . ou pas!&lt;/p&gt;
+                &lt;/aside&gt;
+                &lt;footer&gt;
+                    &lt;h1&gt;sommaire&lt;/h1&gt;
+                    &lt;nav&gt;
+                        &lt;ul&gt;
+                            &lt;li&gt;&lt;a href="#art-1"&gt;p c'est la life&lt;/a&gt;&lt;/li&gt;
+                            &lt;li&gt;&lt;a href="#art-2"&gt;p c'est la life 2&lt;/a&gt;&lt;/li&gt;
+                        &lt;/ul&gt;
+                    &lt;/nav&gt;
+                &lt;/footer&gt;
+                
+            &lt;/article&gt;
+            &lt;article&gt;
+
+                &lt;h1&gt;Faire du lolz avec les serveurs&lt;/h1&gt;
+                &lt;p&gt;...etc...&lt;/p&gt;
+                
+            &lt;/article&gt;
+        &lt;/section&gt;
+    &lt;/main&gt;
+    
+    &lt;aside&gt;
+        &lt;h1&gt;Thème du site&lt;/h1&gt;
+        &lt;p&gt;faire des rouleaux de printemps avec ton cartable et tes ciseaux&lt;/p&gt;
+    &lt;/aside&gt;
+    
+    &lt;footer&gt;
+        &lt;nav&gt;
+            &lt;h1&gt;Carte du site&lt;/h1&gt;
+            &lt;ul&gt;
+                &lt;li&gt;par ici&lt;/li&gt;
+                &lt;li&gt;par là-bas&lt;/li&gt;
+            &lt;/ul&gt;
+        &lt;/nav&gt;
+        &lt;adress&gt;Comment me contacter&lt;/adress&gt;
+        &lt;section class="legal"&gt;
+            &lt;p&gt;informations légales&lt;/p&gt;
+            &lt;p&gt;plus d'informations légales&lt;/p&gt;
+        &lt;/section&gt;
+        
+    &lt;/footer&gt;
+&lt;/body&gt;
+						</pre>
+					</div>
+				</div>
 			</div>
 		</div>
 
@@ -153,19 +399,7 @@ p {
 				</p>
 			</div>
 		</div>
-
-		<div class="row white-box">
-			<div class="col-12">
-				<h1>Les balises HTML 5</h1>
-				<p>
-				<!-- 
-					TODO : 
-					- Parler de main, section, nav, header, footer et article (les présenter très vite fait)
-				-->
-				</p>
-			</div>
-		</div>
-
+        
 		<div class="row white-box">
 			<div class="col-12">
 				<h1>On commence le site projet</h1>
